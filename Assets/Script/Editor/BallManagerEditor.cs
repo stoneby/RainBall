b57e520c65618b2d84660a426268d0507b8fd894
cui xiaoliang <stonebyme@gmail.com>
2013-12-11 17:32:57 +0800

    Fix editor error.

--- Assets/Script/Editor/BallManagerEditor.cs
+++ Assets/Script/Editor/BallManagerEditor.cs
@@ -3,9 +3,11 @@ using UnityEditor;
 [CustomEditor(typeof(BallManager))]
 public class BallManagerEditor : Editor
 {
+    private BallManager manager;
+
     void OnEnable()
     {
-        UpdateManager();
+        manager = target as BallManager;
     }
 
     public override void OnInspectorGUI()
@@ -17,7 +19,6 @@ public class BallManagerEditor : Editor
 
     private void UpdateManager()
     {
-        var manager = target as BallManager;
         manager.MoveDirection = (MoveDirection) EditorGUILayout.EnumPopup("Move Direction", manager.MoveDirection);
         manager.IntersectFactor = EditorGUILayout.Slider("Intersect Factor", manager.IntersectFactor, 0f, 1f);
         manager.DistanceFactor = EditorGUILayout.Slider("Distance Factor", manager.DistanceFactor, 0.8f, 1.2f);
